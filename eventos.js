2817c77a790e097dd4652e219638847d921b98be
federico27jara <123322070+federico27jara@users.noreply.github.com>
2023-01-22 15:03:12 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.getElementById('button_color').addEventListener('click', function(){
+document.body.style.backgroundColor = 'rgb(41, 21, 112)';     
+});
+
+document.getElementById('button_color_reset').addEventListener('click', function(){
+document.body.style.backgroundColor= 'rgb(155, 149, 149';     
+});
